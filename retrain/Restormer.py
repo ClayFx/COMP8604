2e62c9c41d262bac401bccdaf95c88f25e7809c1
Xuan Feng <fengxuan23@qq.com>
2022-08-11 17:46:53 +0800

    add network_vrt

--- retrain/Restormer.py
+++ retrain/Restormer.py
@@ -287,8 +287,8 @@ class Restormer(nn.Module):
 ##---------- StereoRestormer -----------------------
 class StereoRestormer(nn.Module):
     def __init__(self, 
-        inp_channels=3, 
-        out_channels=3, 
+        inp_channels=6, 
+        out_channels=6, 
         dim = 48,
         num_blocks = [4,6,6,8], 
         num_refinement_blocks = 4,
@@ -377,3 +377,122 @@ class StereoRestormer(nn.Module):
 
 
         return out_dec_level1
+
+    def get_flow_2frames(self, x):
+        '''Get flow between frames t and t+1 from x.'''
+
+        b, n, c, h, w = x.size()
+        x_1 = x[:, :-1, :, :, :].reshape(-1, c, h, w)
+        x_2 = x[:, 1:, :, :, :].reshape(-1, c, h, w)
+
+        # backward
+        flows_backward = self.spynet(x_1, x_2)
+        flows_backward = [flow.view(b, n-1, 2, h // (2 ** i), w // (2 ** i)) for flow, i in
+                          zip(flows_backward, range(4))]
+
+        # forward
+        flows_forward = self.spynet(x_2, x_1)
+        flows_forward = [flow.view(b, n-1, 2, h // (2 ** i), w // (2 ** i)) for flow, i in
+                         zip(flows_forward, range(4))]
+
+        return flows_backward, flows_forward
+
+    def get_aligned_image_2frames(self, x, flows_backward, flows_forward):
+        '''Parallel feature warping for 2 frames.'''
+
+        # backward
+        n = x.size(1)
+        x_backward = [torch.zeros_like(x[:, -1, ...]).repeat(1, 4, 1, 1)]
+        for i in range(n - 1, 0, -1):
+            x_i = x[:, i, ...]
+            flow = flows_backward[:, i - 1, ...]
+            x_backward.insert(0, flow_warp(x_i, flow.permute(0, 2, 3, 1), 'nearest4')) # frame i+1 aligned towards i
+
+        # forward
+        x_forward = [torch.zeros_like(x[:, 0, ...]).repeat(1, 4, 1, 1)]
+        for i in range(0, n - 1):
+            x_i = x[:, i, ...]
+            flow = flows_forward[:, i, ...]
+            x_forward.append(flow_warp(x_i, flow.permute(0, 2, 3, 1), 'nearest4')) # frame i-1 aligned towards i
+
+        return [torch.stack(x_backward, 1), torch.stack(x_forward, 1)]
+
+    def forward_vrt(self, x):
+        # x: (N, D, C, H, W)
+
+        # main network
+        if self.pa_frames:
+            x_lq = x.clone()
+
+            # calculate flows
+            flows_backward, flows_forward = self.get_flows(x)
+
+            # warp input
+            x_backward, x_forward = self.get_aligned_image_2frames(x,  flows_backward[0], flows_forward[0])
+            x = torch.cat([x, x_backward, x_forward], 2)
+
+            if self.upscale == 1:
+                # video deblurring, etc.
+                x = self.conv_first(x.transpose(1, 2))
+                x = x + self.conv_after_body(
+                    self.forward_features(x, flows_backward, flows_forward).transpose(1, 4)).transpose(1, 4)
+                x = self.conv_last(x).transpose(1, 2)
+                return x + x_lq
+
+def flow_warp(x, flow, interp_mode='bilinear', padding_mode='zeros', align_corners=True, use_pad_mask=False):
+    """Warp an image or feature map with optical flow.
+
+    Args:
+        x (Tensor): Tensor with size (n, c, h, w).
+        flow (Tensor): Tensor with size (n, h, w, 2), normal value.
+        interp_mode (str): 'nearest' or 'bilinear' or 'nearest4'. Default: 'bilinear'.
+        padding_mode (str): 'zeros' or 'border' or 'reflection'.
+            Default: 'zeros'.
+        align_corners (bool): Before pytorch 1.3, the default value is
+            align_corners=True. After pytorch 1.3, the default value is
+            align_corners=False. Here, we use the True as default.
+        use_pad_mask (bool): only used for PWCNet, x is first padded with ones along the channel dimension.
+            The mask is generated according to the grid_sample results of the padded dimension.
+
+
+    Returns:
+        Tensor: Warped image or feature map.
+    """
+    # assert x.size()[-2:] == flow.size()[1:3] # temporaily turned off for image-wise shift
+    n, _, h, w = x.size()
+    # create mesh grid
+    # grid_y, grid_x = torch.meshgrid(torch.arange(0, h).type_as(x), torch.arange(0, w).type_as(x)) # an illegal memory access on TITAN RTX + PyTorch1.9.1
+    grid_y, grid_x = torch.meshgrid(torch.arange(0, h, dtype=x.dtype, device=x.device), torch.arange(0, w, dtype=x.dtype, device=x.device))
+    grid = torch.stack((grid_x, grid_y), 2).float()  # W(x), H(y), 2
+    grid.requires_grad = False
+
+    vgrid = grid + flow
+
+    # if use_pad_mask: # for PWCNet
+    #     x = F.pad(x, (0,0,0,0,0,1), mode='constant', value=1)
+
+    # scale grid to [-1,1]
+    if interp_mode == 'nearest4': # todo: bug, no gradient for flow model in this case!!! but the result is good
+        vgrid_x_floor = 2.0 * torch.floor(vgrid[:, :, :, 0]) / max(w - 1, 1) - 1.0
+        vgrid_x_ceil = 2.0 * torch.ceil(vgrid[:, :, :, 0]) / max(w - 1, 1) - 1.0
+        vgrid_y_floor = 2.0 * torch.floor(vgrid[:, :, :, 1]) / max(h - 1, 1) - 1.0
+        vgrid_y_ceil = 2.0 * torch.ceil(vgrid[:, :, :, 1]) / max(h - 1, 1) - 1.0
+
+        output00 = F.grid_sample(x, torch.stack((vgrid_x_floor, vgrid_y_floor), dim=3), mode='nearest', padding_mode=padding_mode, align_corners=align_corners)
+        output01 = F.grid_sample(x, torch.stack((vgrid_x_floor, vgrid_y_ceil), dim=3), mode='nearest', padding_mode=padding_mode, align_corners=align_corners)
+        output10 = F.grid_sample(x, torch.stack((vgrid_x_ceil, vgrid_y_floor), dim=3), mode='nearest', padding_mode=padding_mode, align_corners=align_corners)
+        output11 = F.grid_sample(x, torch.stack((vgrid_x_ceil, vgrid_y_ceil), dim=3), mode='nearest', padding_mode=padding_mode, align_corners=align_corners)
+
+        return torch.cat([output00, output01, output10, output11], 1)
+
+    else:
+        vgrid_x = 2.0 * vgrid[:, :, :, 0] / max(w - 1, 1) - 1.0
+        vgrid_y = 2.0 * vgrid[:, :, :, 1] / max(h - 1, 1) - 1.0
+        vgrid_scaled = torch.stack((vgrid_x, vgrid_y), dim=3)
+        output = F.grid_sample(x, vgrid_scaled, mode=interp_mode, padding_mode=padding_mode, align_corners=align_corners)
+
+        # if use_pad_mask: # for PWCNet
+        #     output = _flow_warp_masking(output)
+
+        # TODO, what if align_corners=False
+        return output
